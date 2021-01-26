1b8e24ad48a3b17e4de14f2ab17684856e3a4751
LuisBrito19A <30274509+LuisBrito19A@users.noreply.github.com>
2021-01-25 23:33:38 -0400

    Aquí la tarea del primer corte

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a = 10;
+		int b = 13;
+		int c = 16;
+		
+		//aacb
+		Console.Write(a);
+		Console.Write(a);
+		Console.Write(c);
+		Console.WriteLine(b);
+		
+		Console.WriteLine("----");
+		
+		//bca
+		Console.Write(b);
+		Console.Write(c);
+		Console.WriteLine(a);
+		
+		Console.WriteLine("----");
+
+		//bcbc
+		Console.Write(b);
+		Console.Write(c);
+		Console.Write(b);
+		Console.WriteLine(c);
+		
+		//Hagan su propio estilo :D
+		
+	}
+}
